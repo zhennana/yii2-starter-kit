8356967d06799c829937a9cb006c0a0c5b74f47e
BruceNiu <bruce_bnu@126.com>
2017-02-06 15:47:03 +0800

    add wechat model
    add wechat model

---
+++ common/models/wechat/Wechat.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace common\models\wechat;
+
+use Yii;
+use \common\models\wechat\base\Wechat as BaseWechat;
+
+/**
+ * This is the model class for table "wechat".
+ */
+class Wechat extends BaseWechat
+{
+}
---
+++ common/models/wechat/WechatFans.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace common\models\wechat;
+
+use Yii;
+use \common\models\wechat\base\WechatFans as BaseWechatFans;
+
+/**
+ * This is the model class for table "wechat_fans".
+ */
+class WechatFans extends BaseWechatFans
+{
+}
