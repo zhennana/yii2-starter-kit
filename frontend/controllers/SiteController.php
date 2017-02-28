19574ce007bfbecbd6fac7eac7163f4683f2d0a0
changshaoshuai <854377393@qq.com>
2017-02-28 10:47:59 +0800

    联系我们表单提交数据

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -5,7 +5,7 @@ use Yii;
 use frontend\models\ContactForm;
 use yii\web\Controller;
 use backend\modules\campus\models\ApplyToPlay;
-
+use backend\modules\campus\models\Contact;
 /**
  * Site controller
  */
@@ -26,6 +26,20 @@ class SiteController extends Controller
                   'width' => 100,
                   'minLength' => 4,
                   'maxLength' => 4,
+                  'padding'=>0, 
+                  'offset'=>4, 
+                  'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null
+
+            ],
+            'contact_captcha'=>[
+                'class' => 'yii\captcha\CaptchaAction',
+                  'height' => 40,
+                  'width' => 100,
+                  'minLength' => 4,
+                  'maxLength' => 4,
+                  'padding'=>0, 
+                  'offset'=>4, 
+                  // 'controller'=>'ajax-ontact',
                   'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null
             ],
             'set-locale'=>[
@@ -60,14 +74,44 @@ class SiteController extends Controller
         }
 
     }
-    // public function actionApplyVlidate(){
-    //     if (Yii::$app->request->isAjax) {
-    //         if($model->load(Yii::$app->request->post())){
-    //             return ['status' => $model->save()];
-    //         }else{
-    //             return false;
-    //         }
-    // }
+    /**
+     * 异步提交数据
+     * @return [type] [description]
+     */
+    public function actionAjaxContact(){
+        $model = new Contact;
+        if (Yii::$app->request->isAjax) {
+           
+           Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+          
+            if($model->load(Yii::$app->request->post()) && $model->save()){
+                return ['status' => true];
+            }else{
+               // var_dump($model->getErrors());
+                return ['status'=>false];
+            }
+        }
+        if($model->load(Yii::$app->request->post())){
+            if($model->save()){
+               Yii::$app->getSession()->setFlash('alert', [
+                    'body'=>Yii::t('frontend', 'Thank you for contacting us. We will respond to you as soon as possible.'),
+
+                    'options'=>['class'=>'alert-info']
+                ]);
+                return $this->refresh();
+        }else{
+            Yii::$app->getSession()->setFlash('alert', [
+                    'body'=>\Yii::t('frontend', 'There was an error sending email.'),
+                    'options'=>['class'=>'alert-danger']
+            ]);
+        }
+    }
+        return $this->render('contact',[
+                'model'=>$model
+            ]);
+
+    }
+   
 
     public function actionContact()
     {
