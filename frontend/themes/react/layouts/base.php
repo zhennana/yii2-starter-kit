118a8d7d6870d6667248b92489b66e2a68e234a6
BruceNiu <bruce_bnu@126.com>
2017-03-10 17:29:40 +0800

    add menu
    add menu

--- frontend/themes/react/layouts/base.php
+++ frontend/themes/react/layouts/base.php
@@ -9,6 +9,60 @@ use yii\bootstrap\NavBar;
 $this->beginContent('@frontend/views/layouts/_clear.php')
 ?>
 <div class="wrap">
+<<<<<<< Updated upstream
+=======
+    <?php
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name.' - react',
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]); ?>
+    <?php echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => [
+            ['label' => Yii::t('frontend', 'Home'), 'url' => ['/site/index']],
+            ['label' => Yii::t('frontend', 'About'), 'url' => ['/page/view', 'slug'=>'about']],
+            ['label' => Yii::t('frontend', 'Articles'), 'url' => ['/article/index']],
+            ['label' => Yii::t('frontend', 'Contact'), 'url' => ['/site/contact']],
+            ['label' => Yii::t('frontend', 'Signup'), 'url' => ['/user/sign-in/signup'], 'visible'=>Yii::$app->user->isGuest],
+            ['label' => Yii::t('frontend', 'Login'), 'url' => ['/user/sign-in/login'], 'visible'=>Yii::$app->user->isGuest],
+            [
+                'label' => Yii::$app->user->isGuest ? '' : Yii::$app->user->identity->getPublicIdentity(),
+                'visible'=>!Yii::$app->user->isGuest,
+                'items'=>[
+                    [
+                        'label' => Yii::t('frontend', 'Settings'),
+                        'url' => ['/user/default/index']
+                    ],
+                    [
+                        'label' => Yii::t('frontend', 'Backend'),
+                        'url' => Yii::getAlias('@backendUrl'),
+                        'visible'=>Yii::$app->user->can('manager')
+                    ],
+                    [
+                        'label' => Yii::t('frontend', 'Logout'),
+                        'url' => ['/user/sign-in/logout'],
+                        'linkOptions' => ['data-method' => 'post']
+                    ]
+                ]
+            ],
+            [
+                'label'=>Yii::t('frontend', 'Language'),
+                'items'=>array_map(function ($code) {
+                    return [
+                        'label' => Yii::$app->params['availableLocales'][$code],
+                        'url' => ['/site/set-locale', 'locale'=>$code],
+                        'active' => Yii::$app->language === $code
+                    ];
+                }, array_keys(Yii::$app->params['availableLocales']))
+            ]
+        ]
+    ]); ?>
+    <?php NavBar::end(); ?>
+
+>>>>>>> Stashed changes
     <?php echo $content ?>
 </div>
 
