d2bf50be9e287402bebfd6cb9720021938f437ad
TomaTo <javion_niu@foxmail.com>
2017-04-06 18:18:54 +0800

    修复侧边栏在联系我们页面位移

--- frontend/themes/edu/site/contact.php
+++ frontend/themes/edu/site/contact.php
@@ -11,10 +11,9 @@ $this->title = '联系我们';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="content">
-    <h1><?php echo Html::encode($this->title) ?></h1>
-
-    <div class="row">
+    <article class="article-item">
         <div class="col-sm-8">
+            <h1><?php echo Html::encode($this->title) ?></h1>
             <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
                 <?php echo $form->field($model, 'username') ?>.
                 <?php echo $form->field($model,'phone_number') ?>
@@ -31,6 +30,6 @@ $this->params['breadcrumbs'][] = $this->title;
                 </div>
             <?php ActiveForm::end(); ?>
         </div>
-    <?php echo $this->render('..\page\right-side'); ?>
-    </div>
+        <?php echo $this->render('..\page\right-side'); ?>
+    </article>
 </div>
