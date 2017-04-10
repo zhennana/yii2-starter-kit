41c250fc7933845bbab9dae1b0f9584b48d1fc0c
qq1119094779 <1119094779@qq.com>
2017-04-10 18:17:44 +0800

    导航条完成50%
    导航条完成50%

---
+++ backend/edu-spa/src/components/content/Content.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-col :span="4">
+      <el-menu default-active="0" class="el-menu-vertical-demo" theme="dark">
+        <router-link :to="{path:'StaticPage'}">
+          <el-menu-item index="0">静态页面</el-menu-item>
+        </router-link>
+        <router-link :to="{path:'Article'}">
+          <el-menu-item index="1">文章</el-menu-item>
+        </router-link>
+        <router-link :to="{path:'ArticleClassification'}">
+          <el-menu-item index="2">文章分类</el-menu-item>
+        </router-link>
+        <router-link :to="{path:'TextComponent'}">
+          <el-menu-item index="3">文档组件</el-menu-item>
+        </router-link>
+      </el-menu>
+    </el-col>
+    <div class="child-content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+<script>
+
+</script>
+<style lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ backend/edu-spa/src/components/schoolmanager/CollegeManagement.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-row>
+    <el-col :span="12" class="header-top clearFix">
+      <h2 class="fl">学院管理</h2>
+      <el-button type="info" class="fl append el-icon-plus">创建</el-button>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+
+</script>
+
+<style lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+  .clearFix:after
+    clear:both;
+    display :block;
+    content: '';
+  .clearFix
+    zoom:1;
+  .fr
+    float:right;
+  .fl
+    float:left;
+  .header-top
+    margin-top:20px;
+    margin-left:20px;
+    .append
+      margin-left:10px;
+      span
+        margin-left:5px;
+    h2
+      font-size:36px;
+</style>
