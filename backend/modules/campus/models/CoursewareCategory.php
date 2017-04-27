ad21c0364684cce9de3984e11cd8923f8194ec1b
changshaoshuai854377393 <854377393@qq.com>
2017-04-27 16:17:46 +0800

    课件分类展示

--- backend/modules/campus/models/CoursewareCategory.php
+++ backend/modules/campus/models/CoursewareCategory.php
@@ -27,8 +27,54 @@ public function behaviors()
         return ArrayHelper::merge(
              parent::rules(),
              [
-                  # custom validation rules
+
              ]
         );
+    }
+    public function fields(){
+      return ArrayHelper::merge(
+        parent::fields(),
+        [
+          'updated_at'=>function(){
+              return date('Y-m-d h:i:s',$this->updated_at);
+          },
+          'created_at'=>function(){
+              return date('Y-m-d h:i:s',$this->created_at);
+          },
+          'status_label'=>function(){
+              return self::StatusValueLabel($this->status);
+          },
+          'parent_label'=>function(){
+              return isset($this->parentCoursewareCategory->name) ? $this->parentCoursewareCategory->name : '';
+          }
+        ]);
+    }
+
+    public function CascaderOption(){
+           $model = self::find()->select(['category_id as value','name as label','parent_id'])
+                                      ->where([])
+                                      ->asArray()
+                                      ->all();
+            return self::init_data($model);
+    }
+
+    public  static function init_data($model,$pid = 0,$level = 1,$node = 2){
+              $data = [];
+              if($level > $node ){
+                  return false;
+              }
+              foreach ($model as $key => $value) {
+                  if($value['parent_id'] == $pid){
+                      unset($model[$key]);
+                      $children = self::init_data($model,$value['parent_id'],$level+1);
+                      unset($value['parent_id']);
+                      if(!empty($children)){
+                        $value['children'] = $children;
+                      }
+                      
+                      $data[] = $value;
+                  }
+                } 
+                return $data; 
     }
 }
