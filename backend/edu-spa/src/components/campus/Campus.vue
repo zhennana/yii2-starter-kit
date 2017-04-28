39144d4e3275cc289b171b0d0bdf914bd4adaa69
qq1119094779 <1119094779@qq.com>
2017-04-28 18:57:39 +0800

    学员管理
    学员管理

--- backend/edu-spa/src/components/campus/Campus.vue
+++ backend/edu-spa/src/components/campus/Campus.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <el-col :span="4">
-      <el-menu default-active="0" class="el-menu-vertical-demo" theme="dark">
+    <el-col :span="3">
+      <el-menu default-active="0" class="el-menu-vertical-demo nav-campus" theme="dark" >
         <el-menu-item v-for="menu in menuList" :index="menu.id" :key="menu.id">
           <router-link :to="{path:menu.path}">
             {{menu.title}}
