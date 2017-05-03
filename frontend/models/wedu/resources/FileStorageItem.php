18049bab47d9b4e75188b3e413fa1975682e66dc
changshaoshuai854377393 <854377393@qq.com>
2017-05-03 13:16:31 +0800

    发布分享

---
+++ frontend/models/wedu/resources/FileStorageItem.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace  frontend\models\wedu\resources;
+
+use Yii;
+use frontend\models\base\FileStorageItem as BaseFileStorageItem;
+use yii\helpers\ArrayHelper;
+
+/**
+ * This is the model class for table "file_storage_item".
+ */
+class FileStorageItem extends BaseFileStorageItem
+{
+
+public function behaviors()
+    {
+        return ArrayHelper::merge(
+            parent::behaviors(),
+            [
+                # custom behaviors
+            ]
+        );
+    }
+
+    public function rules()
+    {
+        return ArrayHelper::merge(
+             parent::rules(),
+             [
+                  # custom validation rules
+             ]
+        );
+    }
+}
---
+++ frontend/models/wedu/resources/ShareToFile.php
@@ -0,0 +1,33 @@
+<?php
+namespace frontend\models\wedu\resources;
+
+use Yii;
+use \backend\modules\campus\models\ShareToFile as BaseShareToFile;
+use yii\helpers\ArrayHelper;
+
+/**
+ * This is the model class for table "share_to_file".
+ */
+class ShareToFile extends BaseShareToFile
+{
+
+public function behaviors()
+    {
+        return ArrayHelper::merge(
+            parent::behaviors(),
+            [
+                # custom behaviors
+            ]
+        );
+    }
+
+    public function rules()
+    {
+        return ArrayHelper::merge(
+             parent::rules(),
+             [
+                  # custom validation rules
+             ]
+        );
+    }
+}
