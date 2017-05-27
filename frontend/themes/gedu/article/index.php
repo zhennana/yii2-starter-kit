024710aeba35d97b6e046ed4269fbcdd08fbcfa4
wubinlisha <1037359047@qq.com>
2017-05-27 16:55:20 +0800

    光大web页面

--- frontend/themes/gedu/article/index.php
+++ frontend/themes/gedu/article/index.php
@@ -1,14 +1,59 @@
 <?php
-/* @var $this yii\web\View */
-$this->title = Yii::t('frontend', 'Articles')
+use yii\helpers\Html;
 ?>
-<div id="article-index">
-    <h1><?php echo Yii::t('frontend', 'Articles') ?></h1>
-    <?php echo \yii\widgets\ListView::widget([
-        'dataProvider'=>$dataProvider,
-        'pager'=>[
-            'hideOnSinglePage'=>true,
-        ],
-        'itemView'=>'_item'
-    ])?>
+<div class="gdu-content">
+  <div class="row">
+    <!-- 左边侧边栏 -->
+    <?php
+      echo $this->render('@frontend/themes/gedu/article/common/sidebarnew');
+    ?>
+    <!-- 文章内容部分 -->
+    <div class="col-md-8 ">
+    <div class="box box-widget geu-contentnew">
+            <div class="box-header with-border ">
+              <div class="">
+                <span class=""><i class="fa fa-map-marker margin-r-5 text-purple"></i><a href="#">当前位置:首页>合作交流>友好学校</a></span>
+              </div>
+            </div>
+            <div class="box-body">
+               <div class="box-body">
+              <ul class="todo-list ui-sortable">
+              <?php foreach($dataProvider->getModels() as $key=>$value){?>
+              	<li class="coperli">
+                  <span class="handle ui-sortable-handle">
+                    <i class="fa fa-ellipsis-v"></i>
+                    <i class="fa fa-ellipsis-v"></i>
+                  </span>
+                  <span class="text">
+                  	
+                  	<?php echo Html::a(
+                       $value->title,
+                        ['article/view','id'=>$value['id']],
+                        ['class'=>'','data-method'=>'open',]);
+                    ?>
+                  </span>
+                  <small class="label"><i class="fa fa-clock-o"></i> <?php echo Yii::$app->formatter->asRelativeTime($value->created_at);?></small>
+                  <div class="tools">
+                   <?php echo Html::a(
+                       "详情" ,
+                        ['article/view','id'=>$value['id']],
+                        ['class'=>'','data-method'=>'open',]);
+                    ?>
+                  </div>
+                </li>
+              <?php }?>
+                 <?php 
+		          echo \yii\widgets\LinkPager::widget([
+		            'pagination'=>$dataProvider->pagination,
+		            'options' => ['class' => 'pagination'],
+		        ]);?>
+
+              </ul>
+                   
+      
+            </div>
+            </div>
+      </div>
+    </div>
+  </div>
 </div>
