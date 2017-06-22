c8db99d5684a2c50919235dd3ff7c3778b08e32e
wubin <1037359047@qq.com>
2017-06-22 09:57:48 +0800

    教师风采布局调整

--- frontend/themes/gedu/site/common/elegant.php
+++ frontend/themes/gedu/site/common/elegant.php
@@ -1,6 +1,7 @@
 <?php
 use yii\helpers\Html; 
 ?>
+
 <div class="main-3-school">
     <div class="banner-bottom-left-grids full-length">
         <li class="banner-bottom-left-grid iusto">
@@ -11,6 +12,9 @@ use yii\helpers\Html;
                     <p>学校占地30余亩，建筑面积10000平方米。</p>
                     <?php echo Html::a('了解更多',['site/sights'],['class'=>'btn'])?>
                 </div>
+                <!-- <div class="cap1">
+                    <span> </span>
+                </div> -->
             </div>
         </li>
         <div class="banner-bottom-left-grid txt">
@@ -56,19 +60,19 @@ use yii\helpers\Html;
         </div>
         <li class="banner-bottom-left-grid iusto">
             <div class="port-4 effect-1">
-                <img height="254" src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-4.png?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
-                <div class="text-desc">
-                    <h3>招生咨询处</h3>
-                    <p>光大学校小学部教学体系分三个层次，基础课程、校本课程、兴趣课程。</p>
-                     <?php echo Html::a('了解更多',['site/sights'],['class'=>'btn'])?>
-                </div>
-                <!-- <div class="cap3">
-                    <span> </span>
-                </div> -->
+            <img src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-4.png?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
+            <div class="text-desc">
+                <h3>招生咨询处</h3>
+                <p>光大学校小学部教学体系分三个层次，基础课程、校本课程、兴趣课程。</p>
+                 <?php echo Html::a('了解更多',['site/sights'],['class'=>'btn'])?>
+            </div>
+            <!-- <div class="cap3">
+                <span> </span>
+            </div> -->
             </div>
         </li>
 
-        <div class="banner-bottom-left-grid5 txt" id="txt5">
+        <div class="banner-bottom-left-grid3 txt">
             <a>招生咨询处</a>
             <hr class="hr2" />
         </div>
@@ -76,6 +80,12 @@ use yii\helpers\Html;
             <a>学校操场</a>
             <hr class="hr1" />
         </div>
+        <!-- <div class="banner-bottom-left-grid iusto">
+            <img src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-5.png" alt=" " class="img-responsive" />
+            <div class="cap4">
+                <span> </span>
+            </div>
+        </div> -->
         <li class="banner-bottom-left-grid iusto">
             <div class="port-4 effect-1">
                 <img src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-5.png?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
@@ -92,10 +102,4 @@ use yii\helpers\Html;
         <div class="clearfix"></div>
     </div>
 </div> 
-      
-
-
-
-
-
       
