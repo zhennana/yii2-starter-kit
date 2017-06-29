d0a9b3659186a2f301560e3649d533549e3076fb
wubinyanjiao <1037359047@qq.com>
2017-06-29 18:33:12 +0800

    光大首页调整
    光大首页调整优化

--- frontend/themes/gedu/article/common/sidebarnew.php
+++ frontend/themes/gedu/article/common/sidebarnew.php
@@ -6,10 +6,10 @@ $childNew=!empty($category['childs'])?$category['childs']:$category['child'];
 
 $cateParent=!empty($category['pare_name'])?$category['pare_name']:'光大学校';
 ?><!-- 左边侧边栏 -->
-    <div class="col-md-4">
+    <div class="col-md-3" style="box-sizing: border-box;padding:0">
       
       <div class="" >
-        <div class="gbox">
+        <div class="gbox school-g">
           <h4 class=""><?php echo $cateParent?></h4>
         </div>
         <div class="box-body geu-sidebar">
@@ -39,24 +39,32 @@ $cateParent=!empty($category['pare_name'])?$category['pare_name']:'光大学校'
         <div class="gbox">
           <h4 class="">联系我们</h4>
         </div>
-        <div class="box-body geu-sidebar2" >
+        <div class="box-body geu-sidebar2 box-body2" >
            <div class="box-body">
               <ul>
-                <li><span href="#">地址：<br>
+                <li><p style="line-height: 22px"><span style="display:inline-block;font-size: 14px; font-weight: bold;margin:0;padding:10px 0 5px">地址：</span><br>
                   1、总校 燕郊高新区燕灵路236号（三河二中西邻）<br>
 
                   2、海油大街校区 燕郊高新区海油大街30号（方舟广场往东400米路南）<br>
 
                   3、智慧星校区 燕郊高新区京榆大街1402号（福成国际大酒店对面）
-                </span></li>
-                <li><span>电话：<br>办公室0316-5997070   转6009 <br>
+                </p></li>
+                <li><p style="line-height: 22px"><span style="display:inline-block;font-size: 14px; font-weight: bold;margin:0;padding:10px 0 5px">电话：</span><br>办公室0316-5997070   转6009 <br>
                     小学部办公室         转6003 <br>
                     中学部办公室         转6013  <br>
                     国际部办公室         转6009  <br>
-                    招生部办公室         转6688 </span></li>
-                <li><span>网址：<a href="http://www.guangdaxuexiao.com/">www.guangdaxuexiao.com</a></span></li>
+                    招生部办公室         转6688 </p></li>
+                <li><span><span style="display:inline-block;font-size: 14px; font-weight: bold;margin:0;padding:10px 0 5px">网址：</span><a href="http://www.guangdaxuexiao.com/">www.guangdaxuexiao.com</a></span></li>
               </ul>
             </div>
         </div>
       </div>
     </div>
+    <style type="text/css">
+      .box-body{
+        padding:10px 0;
+      }
+      .box-body2{
+        padding:10px;
+      }
+    </style>
