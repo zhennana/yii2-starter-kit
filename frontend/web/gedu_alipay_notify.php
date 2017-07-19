c029cd118c12583c0d2fd9b23884226a19147575
javion_niu <554348371@qq.com>
2017-07-19 11:22:08 +0800

    支付宝调试

--- frontend/web/gedu_alipay_notify.php
+++ frontend/web/gedu_alipay_notify.php
@@ -52,13 +52,13 @@ if (!isset($arr) || empty($arr)) {
 $alipaySevice = new AlipayTradeService($alipay_config); 
 $alipaySevice->writeLog(var_export($_POST,true));
 $result = $alipaySevice->check($arr);
+$alipaySevice->writeLog($result);
 /* 实际验证过程建议商户添加以下校验。
 1、商户需要验证该通知数据中的out_trade_no是否为商户系统中创建的订单号，
 2、判断total_amount是否确实为该订单的实际金额（即商户订单创建时的金额），
 3、校验通知中的seller_id（或者seller_email) 是否为out_trade_no这笔单据的对应的操作方（有的时候，一个商户可能有多个seller_id/seller_email）
 4、验证app_id是否为该商户本身。
 */
-fileWrite(var_export($_POST,true).'-----------'.$result);
 if($result) {//验证成功
 
     if($_POST['trade_status'] == 'TRADE_SUCCESS' || $_POST['trade_status'] == 'TRADE_FINISHED') {
@@ -92,7 +92,8 @@ if($result) {//验证成功
         $order->payment_status = CourseOrderItem::PAYMENT_STATUS_PAID;
         $order->payment        = CourseOrderItem::PAYMENT_ALIPAY;
         if (!$order->save()) {
-            fileWrite(json_encode($order->Errors()).json_encode($_POST)."\r\n");
+            $alipaySevice->writeLog(json_encode($order->Errors()).json_encode($_POST)."\r\n");
+            // fileWrite(json_encode($order->Errors()).json_encode($_POST)."\r\n");
             exit();
         }
 
