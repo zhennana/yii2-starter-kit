d61d6f4042359987eeb85afc438dc87612fcd656
wubin <1037359047@qq.com>
2017-08-07 17:15:03 +0800

    校园风光图片内容修改

--- frontend/themes/gedu/site/common/elegant.php
+++ frontend/themes/gedu/site/common/elegant.php
@@ -7,11 +7,11 @@ use yii\helpers\Html;
         <li class="banner-bottom-left-grid iusto">
             <div class="port-4 effect-1">
                 <div style="overflow:hidden;">
-                  <img src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-1.png?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
+                  <img src="http://static.v1.guangdaxuexiao.com/1xiaoyuan.jpg?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
                 </div>
                 <div class="text-desc">
-                    <h3 class="img-introduct">内容缺</h3>
-                    <p>内容缺</p>
+                    <h3 class="img-introduct">快乐高效课堂</h3>
+                    <p>温馨快乐校园建设的主阵地，全面提高学生思想道德、科学文化、身心健康全面发展。</p>
                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'btn'])?>
                 </div>
             </div>
@@ -19,11 +19,11 @@ use yii\helpers\Html;
         <li class="banner-bottom-left-grid iusto">
             <div class="port-4 effect-1">
                 <div style="overflow:hidden">
-                    <img src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-4.png?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
+                    <img src="http://static.v1.guangdaxuexiao.com/2xiaoyuan.jpg?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
                 </div>
                 <div class="text-desc">
-                   <h3 class="img-introduct">招生咨询处</h3>
-                    <p>光大学校小学部教学体系分三个层次，基础课程、校本课程、兴趣课程。</p>
+                   <h3 class="img-introduct">陶冶性情的熔炉</h3>
+                    <p>用音乐打开想象的闸门，聆听，最美丽的音乐；倾听，最唯美的境界。</p>
                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'btn'])?>
                 </div>
             </div>
@@ -31,11 +31,11 @@ use yii\helpers\Html;
         <li class="banner-bottom-left-grid iusto">
             <div class="port-4 effect-1">
                 <div style="overflow:hidden">
-                    <img src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-5.png?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
+                    <img src="http://static.v1.guangdaxuexiao.com/3xiaoyuan.jpg?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
                 </div>
                 <div class="text-desc">
-                    <h3 class="img-introduct">教学楼外观</h3>
-                    <p>学校占地30余亩，建筑面积10000平方米。</p>
+                    <h3 class="img-introduct">探索 求知 明理</h3>
+                    <p>在实验室里动手动脑，大胆探索，反复实验，培养孩子广泛的求知欲。</p>
                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'btn'])?>
                 </div>
             </div>
@@ -43,11 +43,11 @@ use yii\helpers\Html;
         <li class="banner-bottom-left-grid iusto">
             <div class="port-4 effect-1">
                 <div style="overflow:hidden">
-                    <img src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-2.png?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
+                    <img src="http://static.v1.guangdaxuexiao.com/4xiaoyuan.jpg?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
                 </div>
                 <div class="text-desc">
-                    <h3 class="img-introduct">内容缺</h3>
-                    <p>内容缺</p>
+                    <h3 class="img-introduct">网络让教育动起来</h3>
+                    <p>网络连接世界，在多彩的电脑课程中，培养孩子的搜索能力、鉴别能力、沟通能力。</p>
                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'btn'])?>
                 </div>
             </div>
@@ -58,11 +58,11 @@ use yii\helpers\Html;
         <li class="banner-bottom-left-grid iusto">
             <div class="port-4 effect-1">
                <div style="overflow:hidden">
-                   <img src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-3.png?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
+                   <img src="http://static.v1.guangdaxuexiao.com/7xiaoyuan.jpg?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
                                     </div>
                 <div class="text-desc">
-                    <h3 class="img-introduct">内容缺</h3>
-                    <p>内容缺</p>
+                    <h3 class="img-introduct">文明宿舍一条心</h3>
+                    <p>温馨整洁的宿舍，是漫漫求学路上一个贴心而又温暖的港湾。</p>
                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'btn'])?>
                 </div>
             </div>
@@ -70,11 +70,11 @@ use yii\helpers\Html;
         <li class="banner-bottom-left-grid iusto">
             <div class="port-4 effect-1">
                 <div style="overflow:hidden">
-                    <img src="http://orh16je38.bkt.clouddn.com/wan.png?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
+                    <img src="http://static.v1.guangdaxuexiao.com/9xiaoyuan.jpg?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
                 </div>
                 <div class="text-desc">
-                    <h3 class="img-introduct">内容缺</h3>
-                    <p>内容缺</p>
+                    <h3 class="img-introduct">中外交流</h3>
+                    <p>在课堂上让感受中外文化思想的碰撞，推动教育全球化，文化多样化。</p>
                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'btn'])?>
                 </div>
             </div>
@@ -82,11 +82,11 @@ use yii\helpers\Html;
         <li class="banner-bottom-left-grid iusto">
             <div class="port-4 effect-1">
                 <div style="overflow:hidden">
-                    <img src="http://orh16je38.bkt.clouddn.com/wanhui.png?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
+                    <img src="http://static.v1.guangdaxuexiao.com/10xiaoyuan.jpg?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
                 </div>
                 <div class="text-desc">
-                    <h3 class="img-introduct">内容缺</h3>
-                    <p>内容缺</p>
+                    <h3 class="img-introduct">生动活泼的乐园</h3>
+                    <p>作为课堂的延伸，为学生提供展示才华和发展特长的舞台，更要让学生体验到学习生活的多姿多彩。</p>
                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'btn'])?>
                 </div>
             </div>
@@ -94,11 +94,11 @@ use yii\helpers\Html;
         <li class="banner-bottom-left-grid iusto">
             <div class="port-4 effect-1">
                 <div style="overflow:hidden">
-                    <img src="http://orh16je38.bkt.clouddn.com/xh.png?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
+                    <img src="http://static.v1.guangdaxuexiao.com/12xiaoyuan.jpg?imageView2/1/w/300/h/300" alt=" " class="img-responsive" />
                 </div>
                 <div class="text-desc">
-                    <h3 class="img-introduct">内容缺</h3>
-                    <p>内容缺</p>
+                    <h3 class="img-introduct">成长道路上的激励与赞赏</h3>
+                    <p>孩子们不放弃一点机会，不停止一日努力，不抱有一丝幻想，成长的道路上你们一定能行！</p>
                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'btn'])?>
                 </div>
             </div>
