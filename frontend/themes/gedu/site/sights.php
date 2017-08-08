f45e119ee8ff8771eaf3665d2c3fb9cf9ce0c6d3
wubin <1037359047@qq.com>
2017-08-08 15:57:53 +0800

    校园风采页面跳转，内容图片更换

--- frontend/themes/gedu/site/sights.php
+++ frontend/themes/gedu/site/sights.php
@@ -4,39 +4,44 @@ use yii\helpers\Html;
 
 $school=[
     '1'=>[
-      'img'=>'http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-2.png?imageView2/1/w/500/h/400',
-      'name'=>'教学楼',
-      'school'=>'光大学校教学楼'
+      'img'=>'http://static.v1.guangdaxuexiao.com/1xiaoyuan.jpg?imageView2/1/w/500/h/400',
+      'name'=>'快乐高效课堂',
+      'school'=>'温馨快乐校园建设的主阵地，全面提高学生思想道德、科学文化、身心健康全面发展。'
       ],
     '2'=>[
-      'img'=>'http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-1.png?imageView2/1/w/500/h/400',
-      'name'=>'教学楼',
-      'school'=>'光大学校教学楼'
+      'img'=>'http://static.v1.guangdaxuexiao.com/2xiaoyuan.jpg?imageView2/1/w/500/h/400',
+      'name'=>'陶冶性情的熔炉',
+      'school'=>'用音乐打开想象的闸门，聆听，最美丽的音乐；倾听，最唯美的境界。'
       ],
     '3'=>[
-      'img'=>'http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-3.png?imageView2/1/w/500/h/400',
-      'name'=>'讲台',
-      'school'=>'光大学校讲台'
+      'img'=>'http://static.v1.guangdaxuexiao.com/3xiaoyuan.jpg?imageView2/1/w/500/h/400',
+      'name'=>'探索 求知 明理',
+      'school'=>'在实验室里动手动脑，大胆探索，反复实验，培养孩子广泛的求知欲。'
       ],
     '4'=>[
-      'img'=>'http://7xsm8j.com2.z0.glb.qiniucdn.com/jiaoxuelou.jpg?imageView2/1/w/500/h/400',
-      'name'=>'教学楼',
-      'school'=>'光大学校教学楼'
+      'img'=>'http://static.v1.guangdaxuexiao.com/4xiaoyuan.jpg?imageView2/1/w/500/h/400',
+      'name'=>'网络让教育动起来',
+      'school'=>'网络连接世界，在多彩的电脑课程中，培养孩子的搜索能力、鉴别能力、沟通能力'
       ],
     '5'=>[
-      'img'=>'http://static.v1.guangdaxuexiao.com/shiyanshi.jpg?imageView2/1/w/500/h/400',
-      'name'=>'实验室',
-      'school'=>'光大学校教学楼'
+      'img'=>'http://static.v1.guangdaxuexiao.com/7xiaoyuan.jpg?imageView2/1/w/500/h/400',
+      'name'=>'文明宿舍一条心',
+      'school'=>'温馨整洁的宿舍，是漫漫求学路上一个贴心而又温暖的港湾。'
       ],
     '6'=>[
-      'img'=>'http://static.v1.guangdaxuexiao.com/librayry.jpg?imageView2/1/w/500/h/400',
-      'name'=>'图书馆',
-      'school'=>'光大学校图书馆'
+      'img'=>'http://static.v1.guangdaxuexiao.com/9xiaoyuan.jpg?imageView2/1/w/500/h/400',
+      'name'=>'中外交流',
+      'school'=>'在课堂上让感受中外文化思想的碰撞，推动教育全球化，文化多样化。'
       ],
     '7'=>[
-      'img'=>'http://static.v1.guangdaxuexiao.com/sushe.jpg?imageView2/1/w/500/h/400',
-      'name'=>'宿舍',
-      'school'=>'光大学校宿舍'
+      'img'=>'http://static.v1.guangdaxuexiao.com/10xiaoyuan.jpg?imageView2/1/w/500/h/400',
+      'name'=>'生动活泼的乐园',
+      'school'=>'作为课堂的延伸，为学生提供展示才华和发展特长的舞台，更要让学生体验到学习生活的多姿多彩。'
+      ],
+      '8'=>[
+      'img'=>'http://static.v1.guangdaxuexiao.com/12xiaoyuan.jpg?imageView2/1/w/500/h/400',
+      'name'=>'成长道路上的激励与赞赏',
+      'school'=>'孩子们不放弃一点机会，不停止一日努力，不抱有一丝幻想，成长的道路上你们一定能行！'
       ],
  ];
 ?>
