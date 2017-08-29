14d4fc6a53ea927484b57eb8b02be51aaf5545f9
zhang901220890821 <15001076921@163.com>
2017-08-29 08:13:35 +0800

    校园风采移动端页面样式修改，首页图片替换
    校园风采移动端页面样式修改，首页图片替换

--- frontend/themes/gedu/site/common/mobile_elegant.php
+++ frontend/themes/gedu/site/common/mobile_elegant.php
@@ -5,57 +5,90 @@ use yii\helpers\Html
     <div class="row ">
         <div class="col-md-3 col-sm-6 col-padding animate-box" >
             <div class="blog-entry">
-                <a href="#" class="blog-img" align="center"><img src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-1.png?imageView2/1/w/300/h/300" class="img-responsive" ></a>
+                <a href="#" class="blog-img" align="center"><img src="http://static.v1.guangdaxuexiao.com/tihuan1.jpg?imageView2/1/w/300/h/300" class="img-responsive" ></a>
                 <div class="desc">
-                    <h3><a href="#">教学楼外观 </a></h3>
+                    <h3><a href="#">快乐高效课堂</a></h3>
                     
-                    <p>学校占地30余亩，建筑面积10000平方米。</p>
+                    <p>温馨快乐校园建设的主阵地，全面提高学生思想道德、科学文化、身心健康全面发展。</p>
                     
-                    <?php echo Html::a('了解更多',['site/sights'],['class'=>'lead'])?>
+                    <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'lead'])?>
                 </div>
             </div>
         </div>
         <div class="col-md-3 col-sm-6 col-padding animate-box" >
             <div class="blog-entry">
-                <a href="#" class="blog-img" align="center"><img src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-2.png?imageView2/1/w/300/h/300" alt=" " class="img-responsive" class="img-responsive" ></a>
+                <a href="#" class="blog-img" align="center"><img src="http://static.v1.guangdaxuexiao.com/wudao.jpg?imageView2/1/w/300/h/300" alt=" " class="img-responsive" class="img-responsive" ></a>
                 <div class="desc">
-                    <h3><a href="#">校园一角 </a></h3>
+                    <h3><a href="#">跳动的精灵 </a></h3>
                     
-                    <p>光大学校小学部教学体系分三个层次，基础课程、校本课程、兴趣课程。</p>
-                     <?php echo Html::a('了解更多',['site/sights'],['class'=>'lead'])?>
+                    <p>在舞蹈的殿堂里，他们舒展沉睡的羽翼，释放天性，自由的飞舞，用心与美来感受爱的真谛。</p>
+                    <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'lead'])?>
                 </div>
             </div>
         </div>
         <div class="col-md-3 col-sm-6 col-padding animate-box" >
             <div class="blog-entry">
-                <a href="#" class="blog-img" align="center"><img src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-3.png?imageView2/1/w/300/h/300" class="img-responsive" ></a>
+                <a href="#" class="blog-img" align="center"><img src="http://static.v1.guangdaxuexiao.com/3xiaoyuan.jpg?imageView2/1/w/300/h/300" class="img-responsive" ></a>
                 <div class="desc">
-                    <h3><a href="#">教室讲台 </a></h3>
+                    <h3><a href="#">探索 求知 明理</a></h3>
                     
-                    <p>光大学校小学部教学体系分三个层次，基础课程、校本课程、兴趣课程。</p>
-                     <?php echo Html::a('了解更多',['site/sights'],['class'=>'lead'])?>
+                    <p>在实验室里动手动脑，大胆探索，反复实验，培养孩子广泛的求知欲。</p>
+                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'lead'])?>
                 </div>
             </div>
         </div>
         <div class="col-md-3 col-sm-6 col-padding animate-box" >
             <div class="blog-entry">
-                <a href="#" class="blog-img" align="center"><img src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-4.png?imageView2/1/w/300/h/300" class="img-responsive" ></a>
+                <a href="#" class="blog-img" align="center"><img src="http://static.v1.guangdaxuexiao.com/4xiaoyuan.jpg?imageView2/1/w/300/h/300" class="img-responsive" ></a>
                 <div class="desc">
-                    <h3><a href="#">招生咨询处 </a></h3>
+                    <h3><a href="#">网络让教育动起来</a></h3>
                     
-                    <p>光大学校小学部教学体系分三个层次，基础课程、校本课程、兴趣课程。</p>
-                     <?php echo Html::a('了解更多',['site/sights'],['class'=>'lead'])?>
+                    <p>网络连接世界，在多彩的电脑课程中，培养孩子的搜索能力、鉴别能力、沟通能力。</p>
+                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'lead'])?>
                 </div>
             </div>
         </div>
         <div class="col-md-3 col-sm-6 col-padding animate-box" >
             <div class="blog-entry">
-                <a href="#" class="blog-img" align="center"><img src="http://7xsm8j.com2.z0.glb.qiniucdn.com/main-3-5.png?imageView2/1/w/300/h/300" class="img-responsive" ></a>
+                <a href="#" class="blog-img" align="center"><img src="http://static.v1.guangdaxuexiao.com/xiaoyuan.jpg?imageView2/1/w/300/h/300" class="img-responsive" ></a>
                 <div class="desc">
-                    <h3><a href="#">学校操场</a></h3>
+                    <h3><a href="#">快乐的元旦</a></h3>
+
+                    <p>微笑的脸蛋甜蜜灿烂，愿每个孩子的明天无限美丽，无限迷人，去获取你光明的未来。</p>
+                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'lead'])?>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-padding animate-box" >
+            <div class="blog-entry">
+                <a href="#" class="blog-img" align="center"><img src="http://static.v1.guangdaxuexiao.com/9xiaoyuan.jpg?imageView2/1/w/300/h/300" class="img-responsive" ></a>
+                <div class="desc">
+                    <h3><a href="#">中外交流</a></h3>
                     
-                    <p>光大学校小学部教学体系分三个层次，基础课程、校本课程、兴趣课程。</p>
-                     <?php echo Html::a('了解更多',['site/sights'],['class'=>'lead'])?>
+                    <p>在课堂上让感受中外文化思想的碰撞，推动教育全球化，文化多样化。</p>
+                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'lead'])?>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-padding animate-box" >
+            <div class="blog-entry">
+                <a href="#" class="blog-img" align="center"><img src="http://static.v1.guangdaxuexiao.com/10xiaoyuan.jpg?imageView2/1/w/300/h/300" class="img-responsive" ></a>
+                <div class="desc">
+                    <h3><a href="#">生动活泼的乐园</a></h3>
+
+                    <p>作为课堂的延伸，为学生提供展示才华和发展特长的舞台，更要让学生体验到学习生活的多姿多彩。</p>
+                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'lead'])?>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-padding animate-box" >
+            <div class="blog-entry">
+                <a href="#" class="blog-img" align="center"><img src="http://static.v1.guangdaxuexiao.com/9xiaoyuan.jpg?imageView2/1/w/300/h/300" class="img-responsive" ></a>
+                <div class="desc">
+                    <h3><a href="#">成长道路上的激励与赞赏</a></h3>
+
+                    <p>孩子们不放弃一点机会，不停止一日努力，不抱有一丝幻想，成长的道路上你们一定能行！</p>
+                     <?php echo Html::a('了解更多',['site/sights','category_id'=>37],['class'=>'lead'])?>
                 </div>
             </div>
         </div>
