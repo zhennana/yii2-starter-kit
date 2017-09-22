b4f706e050899c03c7c890bcbf528eeeec95e72a
javion_niu <554348371@qq.com>
2017-09-22 11:24:24 +0800

    光大接口更新

--- frontend/models/gedu/resources/CourseOrderItem.php
+++ frontend/models/gedu/resources/CourseOrderItem.php
@@ -106,15 +106,14 @@ public function behaviors()
         // 验证订单总价和总课程数，待完善
         if (isset($params['total_price']) && !empty($params['total_price'])) {
             $course = Course::findOne($params['course_id']);
-            if (!isset($course->parent_id) || empty($course->parent_id)) {
-                if ($course->present_price === null) {
+            if ($course && (!isset($course->parent_id) || empty($course->parent_id))) {
+                if ($course->present_price === null || $params['total_price'] != $course->present_price) {
                     $info['errno']   = __LINE__;
-                    $info['message'] = '课程价格异常';
+                    $info['message'] = '现价不合法';
                     return $info; 
                 }
 
-                // 未验证会员价等
-                $params['total_price']  = $course->present_price;
+                // $params['total_price']  = $course->present_price;
                 $params['total_course'] = $course->course_counts;
             }else{
                 $info['errno']   = __LINE__;
