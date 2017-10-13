e5aea4a2dd08b754ed1be0ce1707e21e644bcb1f
javion_niu <554348371@qq.com>
2017-10-13 11:37:48 +0800

    继续支付接口更新

--- frontend/modules/api/v1/controllers/OrderController.php
+++ frontend/modules/api/v1/controllers/OrderController.php
@@ -379,7 +379,7 @@ Receipt: {"Store":"fake","TransactionID":"bc0df36d-13be-4d9f-b9d1-4d980d11c402",
         if ($_POST['payment'] == $modelClass::PAYMENT_ALIPAY) {
             $data = $model->appAlipay();
         }elseif($_POST['payment'] == $modelClass::PAYMENT_WECHAT){
-            // $data = $model->appWechatpay();
+            $data = $model->appWechatpay();
         }
 
         if (isset($data['errno']) && $data['errno'] != 0) {
